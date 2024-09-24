f7b5c8d1270fb50aed4c06a031343687c101dc02
Amit Yadav <156596764+yadavamit0010@users.noreply.github.com>
2024-09-24 23:03:20 +0800

    src files added

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+import { storyblokInit, apiPlugin } from "@storyblok/react";
+
+import Page from "./components/Page";
+import Teaser from "./components/Teaser";
+import Grid from "./components/Grid";
+import Feature from "./components/Feature";
+import HeroSection from "./components/HeroSection";
+import Image from "./components/Image";
+
+storyblokInit({
+  accessToken: "2Kbs08YdMpbtAkWyki3bbAtt",
+  use: [apiPlugin],
+  components: {
+    page: Page,
+    teaser: Teaser,
+    grid: Grid,
+    feature: Feature,
+    hero_section: HeroSection,
+    image: Image    
+  },
+  apiOptions: {
+    // for spaces located in the US or China:
+    // region: "us" or "cn", // you need to specify the region
+    region: ''
+  }
+});
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
